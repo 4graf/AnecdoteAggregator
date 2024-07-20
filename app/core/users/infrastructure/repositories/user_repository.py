015aef86a340d8745bb526afa62dcd155cee749a
4graf <senya.nabatov@mail.ru>
2024-07-20 22:54:13 +0300

    Правки репозитория БД для пользователей

--- app/core/users/infrastructure/repositories/user_repository.py
+++ app/core/users/infrastructure/repositories/user_repository.py
@@ -1,7 +1,7 @@
-from app.core.anecdotes.domain.anecdote_entity import Anecdote
-from app.core.anecdotes.domain.anecdote_repository import AnecdoteRepository
-from app.core.anecdotes.infrastructure.models.anecdote_dao import AnecdoteDao
 from app.core.shared_kernel.db.repository import BaseDBRepository
+from app.core.users.domain.user_entity import User
+from app.core.users.domain.user_repository import UserRepository
+from app.core.users.infrastructure.models.user_dao import UserDao
 
 
 class UserDBRepository(UserRepository, BaseDBRepository[User, UserDao]):
